17c7d7e8a3a81f7611879941c773890611d666fb
Eric Weischedel <yshuttle@gmail.com>
2021-12-17 22:06:55 -0500

    Implement new item

--- src/main/java/com/eweischedel/tutorial/Tutorial.java
+++ src/main/java/com/eweischedel/tutorial/Tutorial.java
@@ -1,10 +1,13 @@
 package com.eweischedel.tutorial;
 
+import com.eweischedel.tutorial.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class Tutorial implements ModInitializer {
+  public static final String MOD_ID = "tutorial";
+
   @Override
   public void onInitialize() {
-
+    ModItems.registerItems();
   }
 }
---
+++ src/main/java/com/eweischedel/tutorial/registry/ModItems.java
@@ -0,0 +1,15 @@
+package com.eweischedel.tutorial.registry;
+
+import com.eweischedel.tutorial.Tutorial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+  public static final Item TOPAZ = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
+  public static void registerItems() {
+    Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "topaz"), TOPAZ);
+  }
+}

1add591094a8c7a28b0b140898166c199abac8d9
Eric Weischedel <yshuttle@gmail.com>
2021-12-18 19:36:35 -0500

    Add loot table and crafting recipe

--- src/main/java/com/eweischedel/tutorial/registry/ModBlocks.java
+++ src/main/java/com/eweischedel/tutorial/registry/ModBlocks.java
@@ -2,6 +2,7 @@ package com.eweischedel.tutorial.registry;
 
 import com.eweischedel.tutorial.Tutorial;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
@@ -13,7 +14,8 @@ public class ModBlocks {
   public static final Block TOPAZ_BLOCK = new Block(
       FabricBlockSettings
           .of(Material.GLASS)
-          .requiresTool()
+          .breakByHand(true)
+          .breakInstantly()
           .strength(5.0f, 30.0f)
           .sounds(BlockSoundGroup.GLASS)
           .luminance(3));
